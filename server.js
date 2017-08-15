13d76095405d5077d986e7994d14511620655ac9
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-15 17:00:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 var column={
      "article-one": {
     title:"Article One",
@@ -62,10 +66,6 @@ app.get('/:articlename',function(req,res){
     res.send(innerHTMLtemplate(column[articlename]));
 });
 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
