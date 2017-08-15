ca1ed2d14c4f32a73a57db392f565095d653adf7
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-15 15:33:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-thre.html'));
+app.get('/ui/article-three',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
