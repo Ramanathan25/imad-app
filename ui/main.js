da9c114d361cb90f5df45bc72cbdbb9c1f92b45e
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-15 18:42:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+var counter1=document.getElementById('counter');
+var count = document.getElementById('count');
+var counter=0;
+counter1.onclick=function(){
+    var request= new XMLHttpRequest();
+    if(this.onreadystatechange==XMLHttpRequest.Done){
+        if(this.status==200){
+            count.innerHTML=this.responsetext;
+        }
+    }
+    request.open('GET','/counter',true);
+}
