d8c87c7e79596e7d6dc267f2b4e3de7f52aa1f2b
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-15 18:49:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var count = document.getElementById('count');
 var counter=0;
 counter1.onclick=function(){
     var request= new XMLHttpRequest();
-    if(this.onreadystatechange==XMLHttpRequest.DONE){
-        if(this.status==200){
-            count.innerHTML=this.responsetext;
+    if(request.onreadystatechange==XMLHttpRequest.DONE){
+        if(request.status==200){
+            count.innerHTML=request.responsetext;
         }
     }
     request.open('GET','https://rsdramanathan.imad.hasura-app.io',true);
