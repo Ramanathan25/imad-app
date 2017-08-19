b64d855cac91786ffd1539c32ea0ef98948eabff
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-19 12:24:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,6 +45,6 @@ submit.onclick=function(){
 
 var name1 = document.getElementById('name');
 var names1=name1.value;
-request.open('GET','http://rsdramanathan.imad.hasura-app.io/submit-name?name='+names1,true);
+request.open('GET','http://rsdramanathan.imad.hasura-app.io/submit-name?name2='+names1,true);
 request.send();
 };
