d16c21a1e2e4e5194d3c17b22c0835e0647aa60a
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-19 12:21:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,7 @@ submit.onclick=function(){
         if(request.status==200){
            var names3=request.responseText;
             names3=JSON.parse(names3);
+            console.log(names3);
     for(var i=0;i<names3.length;i++){
         
         list +='<li>'+names3[i]+'</li>';
