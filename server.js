2c62239e5e5a5fdf0c78e681c09d09d594a1607c
Ramanathan25 <30661181+Ramanathan25@users.noreply.github.com>
2017-08-24 19:22:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/counter',function (req,res){
    res.send(counter.toString());
 });
 app.get('/articles/:articlename',function(req,res){
-   pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName+"'", function(err,result)
+   pool.query("SELECT * FROM article WHERE title='"+ req.params.articlename+"'", function(err,result)
    {
        console.log (pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName+"'"));
        if(err){
